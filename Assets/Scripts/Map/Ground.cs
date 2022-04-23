0b84b8a0e3f2e1e8c73b5a8372b2027a99940d96
chris6713 <chris_6713@yahoo.com>
2022-04-23 05:13:59 -0600

    Build structure has faded preview.

--- Assets/Scripts/Map/Ground.cs
+++ Assets/Scripts/Map/Ground.cs
@@ -12,6 +12,7 @@ public class Ground : InstanceMonoBehaviour<Ground>
 
     LayerMask structuresLayer;
     LayerMask groundLayer;
+    LayerMask ignoreLayer;
 
     GameObject[,] grid;
 
@@ -25,6 +26,7 @@ public class Ground : InstanceMonoBehaviour<Ground>
 
         structuresLayer = LayerMask.NameToLayer("Structures");
         groundLayer = LayerMask.NameToLayer("Ground");
+        ignoreLayer = LayerMask.NameToLayer("Ignore");
 
         groundSize = GetComponent<MeshFilter>().mesh.bounds.size;
 
@@ -32,6 +34,8 @@ public class Ground : InstanceMonoBehaviour<Ground>
         height = (int)groundSize.y;
 
         grid = new GameObject[width, height];
+
+        Debug.Log("grid size width " + width + " height " + height);
 
         RefreshGridObjects();
 
@@ -71,15 +75,13 @@ public class Ground : InstanceMonoBehaviour<Ground>
         var direction = new Vector3(0, -1, 0);
         var start = new Vector3(x, 20, y);
 
-        //Debug.Log("start: " + start + " direction: " + direction);
+        //Debug.DrawRay(start, direction, Color.red, 10);
 
         var ray = new Ray(start, direction);
 
-        //Debug.DrawRay(start, direction, Color.red, 100);
-
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, 100, 1 << groundLayer))
         {
             var obj = hit.transform.gameObject;
 
@@ -100,7 +102,9 @@ public class Ground : InstanceMonoBehaviour<Ground>
         int endx;
         int endy;
 
-        (startx, starty, endx, endy) = GetGridSpaceForObject(obj, obj.transform.localPosition);
+        var ob_pos = obj.transform.localPosition;
+
+        (startx, starty, endx, endy) = GetGridSpaceForObject(obj.GetComponent<MeshFilter>().sharedMesh.bounds.size, new Vector3(ob_pos.x, ob_pos.z, -ob_pos.y));
 
         for (int x = startx; x <= endx; x++)
         {
@@ -113,9 +117,9 @@ public class Ground : InstanceMonoBehaviour<Ground>
         }
     }
 
-    (int, int, int, int) GetGridSpaceForObject(GameObject obj, Vector3 at)
+    (int, int, int, int) GetGridSpaceForObject(Vector3 size, Vector3 at)
     {
-        var size = obj.GetComponent<MeshFilter>().sharedMesh.bounds.size; // get the size
+        //var size = obj.GetComponent<MeshFilter>().sharedMesh.bounds.size; // get the size
         int px, py;
 
         (px, py) = GroundToGridPos(at); // get starting position
@@ -128,7 +132,7 @@ public class Ground : InstanceMonoBehaviour<Ground>
         return (startx, starty, endx, endy);
     }
 
-    Vector3 GridToGroundPos(int x, int y)
+    public Vector3 GridToGroundPos(int x, int y)
     {
         float startx = -groundSize.x / 2;
         float starty = groundSize.y / 2;
@@ -136,35 +140,36 @@ public class Ground : InstanceMonoBehaviour<Ground>
         return new Vector3(startx + x, starty - y);
     }
 
-    (int, int) GroundToGridPos(Vector3 pos)
+    //public (int, int) GroundToGridPos(Vector3 pos)
+    //{
+    //    int x = (int)(50 + pos.x);
+    //    int y = (int)(pos.y > 0 ? 50 - pos.y : 50 + -pos.y);
+
+    //    return (x, y);
+    //}
+
+    public (int, int) GroundToGridPos(Vector3 pos)
     {
         int x = (int)(50 + pos.x);
-        int y = (int)(pos.y > 0 ? 50 - pos.y : 50 + -pos.y);
+        int y = (int)(pos.z > 0 ? 50 - pos.z : 50 + -pos.z);
 
         return (x, y);
     }
 
-    public bool BuildStructure(Item item, Vector3 where)
+    public bool CanBuildStructureAtPos(Vector3 size, Vector3 where)
     {
-        bool success;
-        float ypos;
-        (success, ypos) = GetGroundYAtPos(where.x, where.z);
+        //int gx, gy;
 
-        if (!success) return false;
+        //(gx, gy) = GroundToGridPos(where);
 
-        //where = new Vector3(where.x, ypos, where.z);
-        var groundpos = new Vector3(where.x, where.z * -1, ypos);
+        //int footx = (int)footprintx;
+        //int footy = (int)footprinty;
 
-        var structure = GetStructureForItem(item);
-
-        if (structure == null) return false;
+        //Debug.Log("gx " + gx + " gy " + gy + " where " + where + " footx " + footx + " footy " + footy);
 
         int startx, starty, endx, endy;
-        (startx, starty, endx, endy) = GetGridSpaceForObject(structure, groundpos);
 
-        // check grid to make sure we have room
-
-        bool is_space = true;
+        (startx, starty, endx, endy) = GetGridSpaceForObject(size, where);
 
         for (int x = startx; x <= endx; x++)
         {
@@ -172,18 +177,25 @@ public class Ground : InstanceMonoBehaviour<Ground>
             {
                 if (!ValidPos(x, y) || grid[x, y] != null)
                 {
-                    is_space = false;
-                    break;
+                    Debug.Log("x " + x + " y " + y + " ValidPos " + ValidPos(x, y) + " object " + grid[x, y]?.name);
+                    return false;
                 }
             }
         }
 
-        if (!is_space) return false;
+        return true;
+    }
+
+
+    public bool BuildStructure(GameObject structure, Vector3 size, Vector3 where)
+    {
+        int startx, starty, endx, endy;
+        (startx, starty, endx, endy) = GetGridSpaceForObject(size, where);
 
         structure.transform.SetParent(gameObject.transform);
-        structure.transform.localPosition = groundpos;
+        structure.transform.localPosition = where;
 
-        Debug.Log("spawned " + structure.name + " at " + groundpos);
+        Debug.Log("spawned " + structure.name + " at " + where);
 
         for (int x = startx; x <= endx; x++)
         {
--- Assets/Scripts/Structures/BuildingScreen.cs
+++ Assets/Scripts/Structures/BuildingScreen.cs
@@ -3,17 +3,20 @@
  */
 
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+using System;
 
 public class BuildingScreen : InstanceMonoBehaviour<BuildingScreen>
 {
     [SerializeField] private Material fadeMat;
+    Material oldMat;
+    Color FadeColor = new Color(1, 1, 1, 0.6f);
+    Color ErrorColor = new Color(0.3f, 0.0f, 0.0f, 0.6f);
 
     public bool isOpen { get; private set; } = false;
 
     Item item = null;
     GameObject preview = null;
+    Vector3 previewSize = new Vector3();
 
     protected override void Awake()
     {
@@ -35,16 +38,37 @@ public class BuildingScreen : InstanceMonoBehaviour<BuildingScreen>
             Close(false);
         }
 
+        Vector3 origin = Player.Instance.transform.position;
+        origin += Player.Instance.transform.forward * 7;
+
+        bool success;
+        float ypos;
+        (success, ypos) = Ground.Instance.GetGroundYAtPos(origin.x, origin.z);
+
+        if (!success) ypos = 0;
+
+        preview.transform.localPosition = new Vector3(0, ypos, 7);
+
+        var newpos = Player.Instance.transform.localPosition + Player.Instance.transform.forward * 7;
+
+        bool fits = Ground.Instance.CanBuildStructureAtPos(previewSize, newpos);
+
+        fadeMat.color = fits ? FadeColor : ErrorColor;
+
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            //if (Ground.Instance.BuildStructure(item, origin + transform.forward * 3))
-            //{
-            //    item.Clear();
-            //    QuickSlots.Instance.window.SetBoxSelected(-1);
-            //    QuickSlots.Instance.Refresh();
-            //}
-
-            Close(false);
+            if (fits && success)
+            {
+                if (Ground.Instance.BuildStructure(preview, previewSize, new Vector3(newpos.x, -newpos.z, ypos)))
+                {
+                    preview.GetComponent<Renderer>().material = oldMat;
+                    preview = null;
+                    item.Clear();
+                    QuickSlots.Instance.window.SetBoxSelected(-1);
+                    QuickSlots.Instance.Refresh();
+                    Close(false);
+                }
+            }
         }
     }
 
@@ -60,9 +84,12 @@ public class BuildingScreen : InstanceMonoBehaviour<BuildingScreen>
 
         preview.transform.SetParent(Player.Instance.gameObject.transform);
 
-        preview.GetComponent<Renderer>().material = fadeMat;
+        preview.layer = LayerMask.NameToLayer("Structures");
 
-        preview.transform.localPosition = new Vector3(0, 0, 7);
+        oldMat = preview.GetComponent<Renderer>().material; // save the old material
+        preview.GetComponent<Renderer>().material = fadeMat; // setup the faded material
+
+        previewSize = preview.GetComponent<MeshRenderer>().bounds.size;
 
         QuickSlots.Instance.Close(instant);
 
@@ -83,7 +110,10 @@ public class BuildingScreen : InstanceMonoBehaviour<BuildingScreen>
         isOpen = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-        if (preview != null) Destroy(preview); // get rid of the preview structure if there is one
+        if (preview != null)
+        {
+            Destroy(preview); // get rid of the preview structure if there is one
+        }
 
         Vector3 closedpos = new Vector3(-1000, 0, 0f);
 
